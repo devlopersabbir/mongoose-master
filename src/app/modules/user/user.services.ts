3126da1cfef6e9c0d68f34da9bd1939734691fbd
Shamimayesmin <shamimayesmin144@gmail.com>
2023-05-14 00:24:33 +0600

    create instance

--- src/app/modules/user/user.services.ts
+++ src/app/modules/user/user.services.ts
@@ -2,8 +2,12 @@ import { IUser } from "./user.interface";
 import User from "./user.model";
 
 export const createUserToDB = async (payload : IUser): Promise<IUser> => {
-	const user = new User(payload);
-	await user.save();
+	// creating a new user
+	const user = new User(payload); // User -> class , user ->instance
+	await user.save(); 
+	// user.fullName() //custom instance methods
+	console.log(user.fullName());
+
     return user;
 	// console.log(user);
 };
@@ -15,8 +19,14 @@ export const getUsersFromDB = async(): Promise <IUser[]>=>{
 
 export const getUserByFromDB = async(payload:string): Promise<IUser | null>=>{
 
-	const user = await User.findOne({id: payload}, {name:1});
+	const user = await User.findOne({id: payload}, {name:1, contactNo: 1});
 	return user;
+}
+export const getAdminUsersFromDB = async(payload:string): Promise<IUser | null>=>{
+
+	const user1 = new User() // static user
+	// const user = await User.findOne({id: payload}, {name:1, contactNo: 1});
+	// return user;
 }
 
 
